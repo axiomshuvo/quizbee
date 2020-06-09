733caf607801976140a4a2b149763c177fd0f148
unknown <techshuvo@gmail.com>
2020-06-09 10:11:26 +0600

    add score and play again

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -8,12 +8,15 @@ const QuestionBox = ({question,options, selected}) => {
             {answer.map( (text,index) => (
                 <button key={index} className="answerBtn" onClick={() => {
                     setAnswer([text]);
-                    selected([text]);
+                    selected(text);
                 } } 
                 >
                     {text}
                 </button>
             ) )}
+
+            
+            
         </div>
     )
 }
